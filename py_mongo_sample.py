50c6f480e5701c880f8df4f21e68c7185185b12e
Yuki Endo <yuuki1967@github.com>
2023-06-02 09:59:27 +0900

    first commit

---
+++ py_mongo_sample.py
@@ -0,0 +1,16 @@
+import pymongo
+
+myclent = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclent["mydatabase"]
+mycol = mydb["customers"]
+print(mydb.list_collection_names())
+if "customers" in mydb.list_collection_names():
+    print("The collection exists.") 
+    for x in mycol.find():
+        print(x.get("name"), x.get("email"), x.get("phone"), x.get("credit"))
+else:
+    print("The collection does not exists.")
+    mydict = { "name": "Yuki Endo", "email": "yuuki1967@hoge.com", "phone": "090-1234-5678", "credit": "1111-2222-3333-4444" }
+    x = mycol.insert_one(mydict)
+    print(x.inserted_id)
+    
